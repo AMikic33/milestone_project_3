07f1d634b345115e51792ce34fb9bf30bf7fc41f
AMikic33 <amikic33@hotmail.com>
2020-11-29 16:53:35 +0000

    fix up profile and log out

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
-        return redirect(url_for("user_profile"))
+        return redirect(url_for("user_profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -61,7 +61,8 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    return redirect(url_for("user_profile"))
+                    return redirect(url_for(
+                        "user_profile", username=session["user"]))
             else:
                 # if password doesn't match
                 flash("Credentials incorrect")
@@ -75,12 +76,18 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/user_profile", methods=["GET", "POST"])
-def user_profile():
+@app.route("/user_profile/<username>", methods=["GET", "POST"])
+def user_profile(username):
+    # grab the session user's username from db
     username = mongo.db.users.find_one(
-        {"username"; session["user"]})["username"]
-return render_template("user_profile.html", username=username)
+        {"username": session["user"]})["username"]
 
+    if session["user"]:
+        return render_template("user_profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+    
 @app.route("/logout")
 def logout():
     flash("Logout successful")
