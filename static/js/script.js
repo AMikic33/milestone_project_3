186d54aefe7e75e8493ca26bf79e18f6bad9635f
AMikic33 <amikic33@hotmail.com>
2020-12-01 23:32:06 +0000

    adding input fields to add recipe template

--- static/js/script.js
+++ static/js/script.js
@@ -3,9 +3,3 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
-
-// add recipe - option initialization
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-  });
